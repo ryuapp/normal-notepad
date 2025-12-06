4a9f264803e72d064faee29ce74b58014c50c8bf
ryu <114303361+ryuapp@users.noreply.github.com>
2025-12-06 12:48:02 +0900

    feat: use RICHEDIT50W (#4)

--- src/constants.rs
+++ src/constants.rs
@@ -13,8 +13,12 @@ pub const EM_EXLIMITTEXT: u32 = 0x0435;
 pub const EM_SETTARGETDEVICE: u32 = 0x0448;
 pub const EM_GETTEXT: u32 = 0x000D;
 pub const EM_SETTEXT: u32 = 0x000C;
+pub const EM_SETLANGOPTIONS: u32 = 0x0478;
+pub const EM_GETLANGOPTIONS: u32 = 0x0479;
 pub const ES_MULTILINE: u32 = 0x0004;
 pub const EC_TOPMARGIN: u32 = 0x0002;
+pub const IMF_AUTOFONT: u32 = 0x0002;
+pub const IMF_DUALFONT: u32 = 0x0080;
 
 // Menu command IDs
 pub const ID_FILE_NEW: i32 = 1;
